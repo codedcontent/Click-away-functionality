dc7884177bb9febfaebb35ee6f92006a4c12563b
Ogechukwu Mephors <68477930+codedcontent@users.noreply.github.com>
2023-01-10 21:38:38 +0100

    Starter code uploaded

---
+++ starter-project code/App.jsx
@@ -0,0 +1,20 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="container">
+      {/* Menu container */}
+      <div className="menu__container">
+        {/* Menu button */}
+        <div className="menu">Click Me</div>
+
+        {/* Menu items */}
+        <div className="menu__items">
+          <p className="menu__item">Item 1</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
